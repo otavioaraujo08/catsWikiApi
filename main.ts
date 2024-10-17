1667f1b5529d1cecab0bfc70ae6b4ad616db177e
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-10-17 16:32:36 -0300

    feat: organize functions

--- main.ts
+++ main.ts
@@ -1,20 +1,36 @@
 import "jsr:@std/dotenv/load";
-import {serve} from 'https://deno.land/std@0.136.0/http/server.ts'
+import { serve } from 'https://deno.land/std@0.136.0/http/server.ts';
 import { MongoClient } from "npm:mongodb@6.1.0";
+import mongoose from "npm:mongoose@^6.7";
 
-const client = new MongoClient(Deno.env.get("MONGO_CLIENT"))
+const mongoUri = Deno.env.get("MONGO_CLIENT");
 
-await client.connect();
-
-function requestHandler() {
-    const body = JSON.stringify({ message: "Servidor ativado com sucesso!"})
-
-    return new Response(body, {
-        status: 201,
-        headers: {
-            "content-type": "application/json"
-        }
-    })
+async function connectToMongoDb() {
+    try {
+        const client = new MongoClient(mongoUri);
+        await client.connect();
+        console.log("MongoDB connected successfully.");
+        console.log("Mongoose connection state:", mongoose.connection.readyState);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+    }
 }
 
-serve(requestHandler, {port: 2000})
+function requestHandler(request) {
+    const body = JSON.stringify({ message: "Servidor ativado com sucesso!" });
+
+    return new Response(body, {
+        status: 200,
+        headers: {
+            "content-type": "application/json",
+        },
+    });
+}
+
+async function main() {
+    await connectToMongoDb();
+    serve(requestHandler, { port: 2000 });
+    console.log("Server is running on http://localhost:2000");
+}
+
+main();

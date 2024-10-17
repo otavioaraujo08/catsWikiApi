62b838ea95d056114c42f2e08de66c1e4551eb43
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-10-17 16:05:42 -0300

    feat: add mongo db conection

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
+import "jsr:@std/dotenv/load";
 import {serve} from 'https://deno.land/std@0.136.0/http/server.ts'
+import { MongoClient } from "npm:mongodb@6.1.0";
+
+const client = new MongoClient(Deno.env.get("MONGO_CLIENT"))
+
+await client.connect();
 
 function requestHandler() {
     const body = JSON.stringify({ message: "Servidor ativado com sucesso!"})

6f99f0ee4b43ba7f967d16bc3c0455b4f0121642
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-10-17 15:32:31 -0300

    feat: create server handler

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
-export const helloWorldGuys = (name: string): string => {
-    return `Have a nice week, Mr(s) ${name}`
+import {serve} from 'https://deno.land/std@0.136.0/http/server.ts'
+
+function requestHandler() {
+    const body = JSON.stringify({ message: "Servidor ativado com sucesso!"})
+
+    return new Response(body, {
+        status: 201,
+        headers: {
+            "content-type": "application/json"
+        }
+    })
 }
 
-console.log(helloWorldGuys("Otávio"))
+serve(requestHandler, {port: 2000})
